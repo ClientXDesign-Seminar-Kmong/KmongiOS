7cbbe602efc20fcb0c4ef80ec76a7f149f3f54d8
seu11ee <yslee2504@naver.com>
2020-11-14 13:49:55 +0900

    Initial Project Setting

---
+++ Kmong/Kmong/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kmong
+//
+//  Created by 이예슬 on 11/14/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
