7152b46a0f2f1645ad50695b9b3ddaca3c1258dd
Danna Lee <daeunlee0720@gmail.com>
2020-11-20 20:51:58 +0900

    홈 구조 완료

---
+++ Kmong/Kmong/Sources/HomeVC.swift
@@ -0,0 +1,31 @@
+//
+//  HomeVC.swift
+//  Kmong
+//
+//  Created by DANNA LEE on 2020/11/20.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    @IBOutlet weak var homeTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
