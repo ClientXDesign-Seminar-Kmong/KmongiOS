42f1bb9fd2629e59b9e33f6d1843ea6ddc2220d6
Danna Lee <daeunlee0720@gmail.com>
2020-11-29 02:25:23 +0900

    테이블뷰 셀, 컬렉션 뷰 셀 구현
    최선은 다했으나 에러가 나네 ...

---
+++ Kmong/Kmong/Sources/Cells/HomeRecServiceTableViewCell.swift
@@ -0,0 +1,64 @@
+//
+//  HomeRecServiceTableViewCell.swift
+//  Kmong
+//
+//  Created by DANNA LEE on 2020/11/29.
+//
+
+import UIKit
+
+class HomeRecServiceTableViewCell: UITableViewCell, UICollectionViewDelegate {
+
+    static let identifier = "HomeRecServiceTVC"
+    
+    @IBOutlet var serviceType: UILabel!
+    @IBOutlet var ServiceCollectionView: UICollectionView!
+    
+    var ServiceCells : [String] = ["디자인", "프로그래밍"]
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        ServiceCollectionView.delegate = self
+        ServiceCollectionView.dataSource = self
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
+
+extension HomeRecServiceTableViewCell: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: HomeRecommendedServiceCell.identifier, for: indexPath) as? HomeRecommendedServiceCell else {
+            return UICollectionViewCell()
+        }
+        cell.setImage(imageName: "designerMinju")
+        
+        return cell
+    }
+}
+
+extension HomeRecommendedServiceCell: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout:
+                            UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: collectionView.frame.height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 12
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 11, left: 20, bottom: 16, right: 20)
+    }
+}
--- Kmong/Kmong/Sources/HomeVC.swift
+++ Kmong/Kmong/Sources/HomeVC.swift
@@ -13,8 +13,30 @@ class HomeVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        homeTableView.dataSource = self
     }
 
 }
+
+extension HomeVC : UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: HomeRecServiceTableViewCell.identifier) as? HomeRecServiceTableViewCell else {
+            return UITableViewCell()
+        }
+        return cell
+    }
+}
+
+
+//extension UILabel {
+//    func textFieldTextonBaseLine(text: String?, lineHeight: CGFloat) {
+//        let attributes: [NSAttributedString.Key: Any] = [
+//        .baselineOffset: (lineHeight - font.lineHeight)
+//        ]
+//    }
+//}
+//
