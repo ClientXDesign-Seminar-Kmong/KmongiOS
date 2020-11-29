5cf962be7083b419fc49aef5f3e627f96a123bed
HelloG <zxasd156@icloud.com>
2020-11-29 14:51:41 +0900

    하잉

--- Kmong/Kmong/Sources/VCs/DetailVC.swift
+++ Kmong/Kmong/Sources/VCs/DetailVC.swift
@@ -34,12 +34,11 @@ class DetailVC: UIViewController, UICollectionViewDelegate, UICollectionViewData
     @IBOutlet weak var heartView: UIButton!
     @IBOutlet weak var scrollView: UIScrollView!
     
-    var headerImages: [String] = ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""]
+    var headerImages: [String] = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"]
     
     override func viewDidLoad() {
-        
         super.viewDidLoad()
-
+        
         headerCollectionView.delegate = self
         headerCollectionView.dataSource = self
         scrollView.delegate = self
