8c6e0864d3bd33a93b67cb9929ed1ff9e4e504fd
Unknown <daeunlee0720@gmail.com>
2020-12-03 00:04:28 +0900

    헤더셀 간격 수정

--- Kmong/Kmong/Sources/Cells/HomeTableViewHeaderCell.swift
+++ Kmong/Kmong/Sources/Cells/HomeTableViewHeaderCell.swift
@@ -72,17 +72,17 @@ extension HomeTableViewHeaderCell: UICollectionViewDataSource {
 extension HomeTableViewHeaderCell: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout:
                             UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: collectionView.frame.width, height: 160)
+        return CGSize(width: collectionView.frame.width, height: collectionView.frame.height)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 0
+        return 8
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        return 8
+        return 0
     }
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
+        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
     }
 }
