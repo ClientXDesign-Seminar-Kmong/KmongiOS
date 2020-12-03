d9b8e9bb1196caf149275d627df608c54544e16a
Danna Lee <daeunlee0720@gmail.com>
2020-12-03 20:41:36 +0900

    리뷰평점 별 개수 반영

--- Kmong/Kmong/Sources/VCs/ReviewVC.swift
+++ Kmong/Kmong/Sources/VCs/ReviewVC.swift
@@ -10,17 +10,25 @@ import UIKit
 class ReviewVC: UIViewController {
     
     var reviews : [Review] = []
+    var starAvg : Float = 0.0
+    var starSum : Int = 0
 
     @IBOutlet var reviewTableView: UITableView!
     @IBOutlet var headerView: UIView!
     @IBOutlet var headerReviewCountLabel: UILabel!
     @IBOutlet var writeReviewButton: UIButton!
+    @IBOutlet var star1ImageView: UIImageView!
+    @IBOutlet var star2ImageView: UIImageView!
+    @IBOutlet var star3ImageView: UIImageView!
+    @IBOutlet var star4ImageView: UIImageView!
+    @IBOutlet var star5ImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setReviewData()
         reviewTableView.dataSource = self
+        setAvgStar()
 
         headerReviewCountLabel.font = UIFont(name: "AppleSDGothicNeo-SemiBold", size: 18)
         
@@ -32,12 +40,50 @@ class ReviewVC: UIViewController {
     
     func setReviewData() {
         reviews.append(contentsOf: [
+            Review(id: "abc01234", date: "2020.11.11.", service: "심볼/엠블럼형 로고", review: "친절하게 해주셨어요. 감사합니다.", star: 4),
             Review(id: "eifddnf", date: "2020.12.03.", service: "iOS 앱 개발 외주", review: "빠르네요. 감사합니다.", star: 3),
             Review(id: "diafknd", date: "2020.12.01.", service: "와악", review: "얄머ㅏ아", star: 2),
             Review(id: "eifddnf", date: "2020.12.03.", service: "iOS 앱 개발 외주", review: "빠르네요. 감사합니다.", star: 5),
-            Review(id: "diafknd", date: "2020.12.01.", service: "와악", review: "얄머ㅏ아", star: 3),
-            Review(id: "abc01234", date: "2020.11.11.", service: "심볼/엠블럼형 로고", review: "친절하게 해주셨어요. 감사합니다.", star: 4)
+            Review(id: "diafknd", date: "2020.12.01.", service: "와악", review: "얄머ㅏ아", star: 3)
         ])
+    }
+    
+    func setAvgStar() {
+        for i in 0...reviews.count-1 {
+            starSum += reviews[i].star
+        }
+        starAvg = Float(starSum)/Float(reviews.count)
+//        print(reviews.count, starSum, starAvg)
+        
+        if starAvg < 0.5 {
+            star1ImageView.image = UIImage(named: "icStarReviewUnselected")
+        }
+        if starAvg >= 0.5 {
+            star1ImageView.image = UIImage(named: "icStarReviewSelected")
+        } else {
+            star1ImageView.image = UIImage(named: "icStarReviewUnselected")
+        }
+        if starAvg >= 1.5 {
+            star2ImageView.image = UIImage(named: "icStarReviewSelected")
+        } else {
+            star2ImageView.image = UIImage(named: "icStarReviewUnselected")
+        }
+        if starAvg >= 2.5 {
+            star3ImageView.image = UIImage(named: "icStarReviewSelected")
+        } else {
+            star3ImageView.image = UIImage(named: "icStarReviewUnselected")
+        }
+        if starAvg >= 3.5 {
+            star4ImageView.image = UIImage(named: "icStarReviewSelected")
+        } else {
+            star4ImageView.image = UIImage(named: "icStarReviewUnselected")
+        }
+        if starAvg >= 4.5 {
+            star5ImageView.image = UIImage(named: "icStarReviewSelected")
+        } else {
+            star5ImageView.image = UIImage(named: "icStarReviewUnselected")
+        }
+        return
     }
 
 
