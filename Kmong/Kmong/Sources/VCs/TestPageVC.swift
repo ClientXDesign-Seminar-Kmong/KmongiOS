26e49a0897fe8c13c33866fa1ec53b2d8e082f9e
seu11ee <yslee2504@naver.com>
2020-12-03 21:44:01 +0900

    detail merge?

---
+++ Kmong/Kmong/Sources/VCs/TestPageVC.swift
@@ -0,0 +1,29 @@
+//
+//  TestPageVC.swift
+//  Kmong
+//
+//  Created by 이예슬 on 2020/12/03.
+//
+
+import UIKit
+
+class TestPageVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
